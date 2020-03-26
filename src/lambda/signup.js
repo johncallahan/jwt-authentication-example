d762004d12a4833c4b8de8853af9db76046035c8
johncallahan <645344+johncallahan@users.noreply.github.com>
2020-03-26 14:28:36 -0400

    Update signup.js

--- src/lambda/signup.js
+++ src/lambda/signup.js
@@ -33,9 +33,9 @@ export async function handler(event) {
       password: passwordHash
     });
     console.log("reached 7");
-    cookie = createJwtCookie(insertedId, email);
+    const cookie = createJwtCookie(insertedId, email);
     console.log("reached 8");
-    body = JSON.stringify({ id: insertedId, email });
+    const body = JSON.stringify({ id: insertedId, email });
     console.log("reached 9");
     return {
       statusCode: 200,
